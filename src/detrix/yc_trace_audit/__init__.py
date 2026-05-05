35ec0252d7eceffa71233f9d73debc8b97dc5608
gabegraves <gabegraves201@gmail.com>
2026-05-05 03:22:11 -0400

    Revert "Build YC trace audit source registry"
    This reverts commit 1d94c6ac0e3d37b0ef9a0d6e62cf36f7c4f0761b.

--- src/detrix/yc_trace_audit/__init__.py
+++
@@ -1,5 +0,0 @@
-"""YC trace audit runbook harness."""
-
-from detrix.yc_trace_audit.projects import AUDIT_WINDOW, CORE_PROJECTS, project_for_path
-
-__all__ = ["AUDIT_WINDOW", "CORE_PROJECTS", "project_for_path"]
